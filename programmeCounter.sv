2aea2a8340af8591cc793271595ff25ab85182bc
fjsjrobinyuan <102639310+fjsjrobinyuan@users.noreply.github.com>
2023-10-20 18:03:18 +0100

    Update programmeCounter.sv

--- programmeCounter.sv
+++ programmeCounter.sv
@@ -1,20 +1,21 @@
 module programmeCounter(
-    input logic clk,          // Clock input
-    input logic reset,        // Reset signal
-    input logic beq,          // BEQ control signal
-    input logic [31:0] pc_in, // New program counter value
-    input logic [31:0] branch_target, // Target address for the branch
-    output logic [31:0] pc    // Current program counter value
+    input logic clk,
+    input logic [31:0] newPC,
+    output logic [31:0] PC,
+    input logic branchEnable
 );
-
-    always_ff @(posedge clk, posedge reset) begin
-        if (reset) begin
-            pc <= 32'b0; // Reset to address 0
-        end else if (beq && (pc == branch_target)) begin
-            pc <= pc_in; // Branch taken, update PC with new value
-        end else begin
-            pc <= pc + 32'h1; // Increment PC by 1
+    logic [31:0] value;
+    
+    always_ff @(posedge clk) begin
+        if (branchEnable)
+            value <= newPC;
+        else begin
+            if (value == 32'hFFFFFFFF)
+                value <= 32'h00000000;
+            else
+                value <= value + 1;
         end
     end
-
+    
+    assign PC = value;
 endmodule
