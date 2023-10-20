58c4dc1e493087ef791d940f29057c30610a648b
fjsjrobinyuan <102639310+fjsjrobinyuan@users.noreply.github.com>
2023-10-20 18:02:25 +0100

    Update TopLevel.sv

--- TopLevel.sv
+++ TopLevel.sv
@@ -1,14 +1,39 @@
-module TopLevel;
-    parameter PARAM_TOP = 16; 
-    wire [7:0] dataA, dataB; 
-    instructionMemory #(.width(32),.address(16)) instrMem (
-        .writeEnable(instrMemWE),
-        .fetchEnable(instrMemFE),
-        .instructionInput(instrMemIn),
-        .instructionOutput(instrMemOut)
-    );
-    datapathController datapathcontrol (
-        .dataIn(dataIn),
-        .dataOut
-    )
+module TopLevel (
+                  input logic [63:0] instruction,
+                  input logic view,
+                  input logic [7:0] viewAddress,
+                  output logic [31:0] dataContents,
+                  output logic overflow
+);
+datapathController dpct(.instruction(instruction),
+                        .ALUCompare(alu.compareResult),
+                        .resetRegisterFile,
+                        .resetMemory,
+                        .registerFileRead,
+                        .registerFileWrite,
+                        .memoryRead,
+                        .memoryWrite,
+                        .sourceSelect(aluss.sourceSelect),
+                        .PCChangeEnable,
+                        .computationSelect(alu.aluControl),
+                        .target,
+                        .sourceA,
+                        .sourceB,
+                        .immediate(aluss.immediate)
+                        );
+ALUSourceSelect aluss(.immediate(dpct.immediate),
+                      .operandA(sourceA),
+                      .operandB(sourceA),
+                      .sourceSelect(dpct.sourceSelect),
+                      .resultA(alu.operandA),
+                      .resultB(alu.operandB)
+                      );
+ALU alu(.operandA(aluss.resultA),
+        .operandB(aluss.resultB),
+        .aluControl(dpct.computationSelect),
+        .result,
+        .compareResult(dpct.ALUCompare),
+        .overflow(overflow)
+        );                 
+
 endmodule
